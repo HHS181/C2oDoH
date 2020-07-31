e8ffcbe0e0273ac205fb8947b4b230052bb4a402
181 <18160883@student.hhs.nl>
2020-07-31 14:16:08 +0200

    Added hosname change

--- Internal_Resolver/IRpostscript.sh
+++ Internal_Resolver/IRpostscript.sh
@@ -2,9 +2,13 @@
 
 SERVER="https://raw.githubusercontent.com/HHS181/C2oDoH/master/Internal_Resolver"
 
+# Change hostname
+sed -i 's/debian/InternalResolver/g' /etc/hostname
+sed -i 's/debian/InternalResolver/g' /etc/hosts
+
 # Install BIND
 apt-get update
-apt-get install bind9 bind9utils bind9-doc bind9-host
+apt-get -y install bind9 bind9utils bind9-doc bind9-host
 
 # Download BIND configs
 curl $SERVER/named.conf.options -o /etc/bind/named.conf.options
