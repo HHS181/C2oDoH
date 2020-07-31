64ba9c68fc9699e947b4e9b1ffe2ab5bb6d6dffb
181 <18160883@student.hhs.nl>
2020-07-31 12:55:43 +0200

    Initial commit

---
+++ Internal_Resolver/IRpostscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SERVER="https://raw.githubusercontent.com/HHS181/C2oDoH/master/Internal_Resolver"
+
+# Install BIND
+apt-get update
+apt-get install bind9 bind9utils bind9-doc bind9-host
+
+# Download BIND configs
+curl $SERVER/named.conf.options -o /etc/bind/named.conf.options
+
+# Start the BIND service on boot
+systemctl enable bind9
