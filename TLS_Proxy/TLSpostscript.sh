2f00c9c26838dd0dd9572f57b3d51ba13156b6e7
181 <18160883@student.hhs.nl>
2020-08-05 11:54:51 +0200

    Added network configs

--- TLS_Proxy/TLSpostscript.sh
+++ TLS_Proxy/TLSpostscript.sh
@@ -10,8 +10,17 @@ sed -i 's/debian/TLSProxy/g' /etc/hosts
 apt-get update
 apt-get -y install mitmproxy
 
+# Run mitmproxy for certificate generation
+timeout 60s mitmproxy
+
+# Network configuration for mitmproxy
+curl $SERVER/sysctl.conf -o /etc/sysctl.conf
+mkdir /etc/iptables
+curl $SERVER/rules.v4 -o /etc/iptables/rules.v4
+(echo "@reboot sleep 10 && iptables-restore < /etc/iptables/rules.v4") | crontab -
+
 # Setup simple HTTP server for certificate downloads
-(echo "@reboot sleep 30 && python3 -m http.server") | crontab -
+(echo "@reboot sleep 10 && python3 -m http.server") | crontab -
 
 # Cleanup
 rm -rf /root/temp
