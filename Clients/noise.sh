a5e17a2565e3a64c2983af6780b581c4eade6d9e
181 <18160883@student.hhs.nl>
2020-09-07 14:07:36 +0200

    Initial commit

---
+++ Clients/noise.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Terminate all script processes on CTRL+C
+stopScripts() {
+    pkill youtube-dl
+    pkill partyloud
+    rm 10\ Hours*
+    cd -
+}
+
+trap 'stopScripts' SIGINT
+
+# Simulate YouTube streaming
+youtube-dl -q -r 48K https://www.youtube.com/watch?v=Tpk4q_Zo2ws &
+
+# Simulate web browsing
+cd /home/user/PartyLoud
+./partyloud
